cb0ab7266130f9ea286fbd7c5190d327434a0388
LostKningc <61621829+LostKningc@users.noreply.github.com>
2024-07-12 13:35:52 +0800

    Delete IRtoAsm directory

--- IRtoAsm/unitTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/IRtoAsm.hpp"
-#include <fstream>
-
-int main(){
-    std::string inputfile = "test.kp";
-    std::string outputfile = "test.s";
-    IRanalysis(inputfile, outputfile);
-    return 0;
-}
